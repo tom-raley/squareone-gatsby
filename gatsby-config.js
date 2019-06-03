78bf852c2cc73d70d67f0e32ea4885d304629690
tom-raley <tom.raley@gmail.com>
2019-06-03 17:02:05 -0500

    imported legacy code and began refactoring

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,18 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/dist/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+          family: `Raleway`,
+          variants: [`400`, `700`]
+          },
+        ],
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,7 +35,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+         // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

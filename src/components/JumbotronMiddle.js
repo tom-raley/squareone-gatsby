f3040cf7edb21d07eda23d10df2268bc6fb998aa
tom-raley <tom.raley@gmail.com>
2019-06-05 13:37:04 -0500

    button fix #3

--- src/components/JumbotronMiddle.js
+++ src/components/JumbotronMiddle.js
@@ -25,7 +25,7 @@ class JumbotronMiddle extends Component {
             <Img className="diagonal-logo img-fluid" fluid={diagonalLogo} alt="diagonal logo" id="diagonal-logo" />
           </div>
           <div id="pinterest-button">
-            <a data-pin-do="buttonPin" href="https://www.pinterest.com/pin/create/button/?url=https%3A%2F%2Fwww.squareonemission.com&media=https%3A%2F%2Fsquareonemission.com%2Fresources%2Fimages%2Fdiagonal-logo.jpg&description=Reusable%20Eco-Friendly%20Sandwich%20%26%20Snack%20Bags"></a>
+            <a data-pin-do="buttonPin" style={{width: '49.5px !important'}} href="https://www.pinterest.com/pin/create/button/?url=https%3A%2F%2Fwww.squareonemission.com&media=https%3A%2F%2Fsquareonemission.com%2Fresources%2Fimages%2Fdiagonal-logo.jpg&description=Reusable%20Eco-Friendly%20Sandwich%20%26%20Snack%20Bags"></a>
           </div>
           <div data-aos="fade-left" data-aos-duration="2000" className="desktop-mission">
             <p>We design one-of-a-kind, sustainable products. Simplicity, safety and superior durability are featured in high quality materials and craftsmanship. <br /><br/>"U are one" that will make a difference today. Join us on our mission to reuse.<br /><br />Meet us at Square One.</p>
